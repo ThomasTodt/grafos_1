d8b6e1f75dce7afb88a7b55d5b0a4a981d6a48de
Thomas Todt <thomas.bianchi.todt@gmail.com>
2022-07-25 02:01:08 -0300

    complemento, matriz adjacencias, triangulos, conexo

--- grafos1/grafo.c
+++ grafos1/grafo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "grafo.h"
 
+typedef struct {
+  Agrec_t header;
+  int contado;
+} info_vertice;
+
+typedef struct {
+  Agrec_t header;
+  int criada;
+} info_aresta;
+
 //------------------------------------------------------------------------------
 grafo le_grafo(void) 
 {
@@ -9,13 +19,12 @@ grafo le_grafo(void)
 //------------------------------------------------------------------------------
 void destroi_grafo(grafo g) 
 {
-  //return agclose(g);
+  return agclose(g);
 }
 //------------------------------------------------------------------------------
 grafo escreve_grafo(grafo g) 
 {
-  agwrite(g, stdout);
-  return g;
+  return agwrite(g, stdout);
 }
 
 // -----------------------------------------------------------------------------
@@ -85,7 +94,7 @@ int grau_minimo(grafo g)
 }
 
 // -----------------------------------------------------------------------------
-int grau_medio(grafo g)
+int grau_medio(grafo g) 
 {
   vertice atual = agfstnode(g);
   int total = 0;
@@ -103,9 +112,27 @@ int grau_medio(grafo g)
 }
 
 // -----------------------------------------------------------------------------
-int regular(grafo g)
+int regular(grafo g) 
 {
-  return 0;
+  
+  vertice atual = agfstnode(g);
+  int grauReg = grau(atual, g);
+
+  vertice ultimo = aglstnode(g);
+
+
+  while (atual != ultimo)
+  {
+    int grauAtual = grau(atual, g);
+
+    if (grauAtual != grauReg)
+      return 0;
+
+    atual = agnxtnode(g, atual);
+  }
+
+
+  return 1;
 }
 
 // -----------------------------------------------------------------------------
@@ -115,9 +142,48 @@ int completo(grafo g) {
 }
 
 // -----------------------------------------------------------------------------
-int conexo(grafo g) {
-  
+int backtrack_conexo(grafo g, vertice atual, int *contador_vertices) // funcao que auxilia a funcao "conexo"
+{
+  if (contador_vertices == 0)
+    return 1;
+
+  info_vertice *info;
+  info = aggetrec(atual, "info_vertice", TRUE);
+
+  if (info->contado == 0) // contabiliza o vertice
+  {
+    info->contado = 1;
+    *contador_vertices--;
+
+
+    for (vertice v = agnxtnode(g, atual); v; v = agnxtnode(g,v))
+    {
+      if ( agedge(g, atual, v, NULL, 0) );
+      {
+        if ( backtrack_conexo(g, v, contador_vertices) == 1 )
+          return 1;
+      }
+    }
+
+  }
+
+
   return 0;
+}
+
+int conexo(grafo g) 
+{
+  info_vertice *info;
+
+  for (vertice v = agfstnode(g); v; v = agnxtnode(g,v))
+  {
+    info = agbindrec(v, "info_vertice", sizeof(info_vertice), TRUE);
+    info->contado = 0;
+  }
+
+  int contador_vertices = n_vertices(g);
+
+  return backtrack_conexo(g, agfstnode(g), &contador_vertices);
 }
 
 // -----------------------------------------------------------------------------
@@ -127,20 +193,146 @@ int bipartido(grafo g) {
 }
 
 // -----------------------------------------------------------------------------
-int n_triangulos(grafo g) {
+void multiplica_matriz_adjacencia(int **A, int **B, int **C, int tamanho)
+{
+  for (int i=0; i < tamanho; i++)
+  {
+    for (int j=0; j < tamanho; j++)
+    {
+      C[i][j] = 0;
+      for (int k=0; k < tamanho; k++)
+        C[i][j] += A[i][k] * B[k][j];
+    }
+  }
+}
+
+int n_triangulos(grafo g) 
+{
+  int **mat = matriz_adjacencia(g);
+  int **mat2, **mat3;
+
+  int tamanho = n_vertices(g);
+
+  mat2 = malloc (tamanho * sizeof (int*)) ;
+  for (int i=0; i < tamanho; i++)
+    mat2[i] = malloc (tamanho * sizeof (int)) ;
+
+  mat3 = malloc (tamanho * sizeof (int*)) ;
+  for (int i=0; i < tamanho; i++)
+    mat3[i] = malloc (tamanho * sizeof (int)) ;
+
+
+  multiplica_matriz_adjacencia(mat, mat, mat2, tamanho);
+  multiplica_matriz_adjacencia(mat, mat2, mat3, tamanho);
+
+
+  int traco = 0 ;
+  for (int i=0; i < tamanho; i++)
+    traco += mat3[i][i];
+
+
+  // liberar matrizes
+  for (int i=0; i < tamanho; i++)
+  {
+    free(mat[i]);
+    free(mat2[i]);
+    free(mat3[i]);
+  }
+  free(mat);
+  free(mat2);
+  free(mat3);
   
-  return 0;
+  return traco;
 }
 
 // -----------------------------------------------------------------------------
-int **matriz_adjacencia(grafo g) {
+int **matriz_adjacencia(grafo g) 
+{
+  Agnode_t *n;
+  Agnode_t *m;
+  Agedge_t *e;
+  int i, j;
+  int num_vertices = n_vertices(g);
+
+  int **mat;
+
+  mat = malloc (num_vertices * sizeof (int*)) ;
+
+  for (i=0; i < num_vertices; i++)
+    mat[i] = malloc (num_vertices * sizeof (int)) ;
   
-  return NULL;
+  i=0;
+  j=0;
+  for (n = agfstnode(g); n; n = agnxtnode(g,n)) {
+    j=0;
+    for (m = agfstnode(g); m; m = agnxtnode(g,m)) {
+      e = agedge(g,n,m,NULL,FALSE);
+
+
+      if(e != NULL){
+        mat[i][j]=1;
+        printf("1");
+      }
+      else{
+        mat[i][j]=0;
+        printf("0");
+      }
+        
+      j++;
+    }
+
+    printf("\n");
+    i++;
+  }
+  
+  return mat;
 }
 
 // -----------------------------------------------------------------------------
-grafo complemento(grafo g) {
-  
-  return NULL;
-}
+grafo complemento(grafo g)
+{
+  info_aresta *info;
 
+  grafo h = agread( agwrite(g, stdin), stdin );
+
+  // inicializa arestas (records)
+  for (vertice v = agfstnode(h); v; v = agnxtnode(h,v))
+  {
+    for (Agedge_t *a = agfstedge(h, v); a; a = agnxtedge(h, a, v)) // inicializa mais de uma vez a mesma aresta?
+    {
+      info = agbindrec(v, "info_aresta", sizeof(info_aresta), TRUE);
+      info->criada = 0 ;
+    }
+  }
+
+  // cria arestas faltantes
+  for (vertice v = agfstnode(h); v; v = agnxtnode(g,v))
+  {
+    for (vertice v2 = agfstnode(h); v2; v2 = agnxtnode(h, v2))
+    {
+      if (v != v2)
+      {
+        if ( ! agedge(h, v, v2, NULL , 0) ) // se nao existe aresta entre os vertices, faça:
+        {
+          Agedge_t *a = agedge(h, v, v2, NULL, 1); // cria aresta
+          info = agbindrec(a, "info_aresta", sizeof(info_aresta), TRUE);
+          info->criada = 1 ;
+        }
+      }
+    }
+  }
+
+  // deleta arestas originais
+  for (vertice v = agfstnode(h); v; v = agnxtnode(h, v))
+  {
+    for (vertice v2 = agfstnode(h); v2; v2 = agnxtnode(h, v2))
+    {
+      Agedge_t *a = agedge(h, v, v2, NULL, 0);
+      info = aggetrec( a, "info_aresta", TRUE );
+      if( info->criada == 0) // se aresta pertence ao grafo original:
+        agdeledge(h, a);
+    }
+  }
+  
+  return h;
+}
